6bcd4a38d76c946a7727ede5213b7580e11fbe1e
manjeet00129 <165686166+manjeet00129@users.noreply.github.com>
2024-08-09 00:18:12 +0530

    Update practical3.py

--- practical3.py
+++ practical3.py
@@ -7,7 +7,6 @@ print(ispangram("The quick brown fox jumps over the lazy dog"))
 
 #practical3b:print the elements of the list that of the list that are less than5.
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
 for i in a:
     if i < 5:
         print(i)
