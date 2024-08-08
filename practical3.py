7c826678f2afc8daeaf279c7ea333915a8e77c57
manjeet00129 <165686166+manjeet00129@users.noreply.github.com>
2024-08-09 00:17:41 +0530

    Update practical3.py

--- practical3.py
+++ practical3.py
@@ -1,15 +1,11 @@
 #practical3a:all latters of alphabets
-
 import string
-
 def ispangram(str1, alphabet=string.ascii_lowercase):
     alphaset = set(alphabet)
     return alphaset <= set(str1.lower())
-
 print(ispangram("The quick brown fox jumps over the lazy dog"))
 
 #practical3b:print the elements of the list that of the list that are less than5.
-
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 for i in a:
