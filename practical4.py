0e71482e140be0699da86b3d1b0958607c129e78
manjeet00129 <165686166+manjeet00129@users.noreply.github.com>
2024-09-05 15:13:27 +0530

    Create practical4.py

---
+++ practical4.py
@@ -0,0 +1,11 @@
+#practical4a:Check if two lists have any common elements.
+def find_common(st1, st2):
+    res = False
+    for x in st1:
+        for y in st2:
+            if x == y:
+                res = True
+    return res
+
+print(find_common([4, 6, 8, 10, 12], [5, 7, 8, 10, 11]))  # Output: True
+print(find_common([3, 5, 7, 9, 11], [2, 4, 6, 8]))        # Output: False
