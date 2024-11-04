11d060f4d26eabb31efd2c34f92bfe21b980c11d
manjeet00129 <165686166+manjeet00129@users.noreply.github.com>
2024-11-05 03:00:51 +0530

    Update practical7.py

--- practical7.py
+++ practical7.py
@@ -1,4 +1,4 @@
-#B#class that store information
+#A#class that store information
 class student:
   def info(self, studname,studaddress, studclass):
      print("name:", studname)
@@ -24,3 +24,33 @@ t.s1()
 
 # Call the method from derived class
 t.s2()
+
+
+#c) Create a class called Numbers, which has a single class attribute called MULTIPLIER, and a constructor which takes the parameters x and y (these should all be numbers).
+ class Numbers:
+    MULTIPLIER=3
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def add(self):
+        return self.x+self.y
+    @classmethod
+    def multiply(cls,a):
+        return cls.MULTIPLIER*a
+    @staticmethod
+    def subtract(b,c):
+        return b-c
+    @property
+    def value(self):
+        return(self.x,self.y)
+    @value.setter
+    def value(self,xy_tuple):
+        self.x,self.y=xy_tuple
+    @value.deleter
+    def value(self):
+        del self.x
+        del self.y
+T=Numbers(2,4)
+print(T.add())
+print(T.multiply(2))
+print(Numbers.subtract(4,3))
